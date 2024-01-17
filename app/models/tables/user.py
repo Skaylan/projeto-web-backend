91064576e740e7fb4b2b967cf306553b78413f0e
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-01-16 23:56:24 -0300

    Feat: Modelagem da coluna User

--- app/models/tables/user.py
+++ app/models/tables/user.py
@@ -6,3 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.Text, unique=False, nullable=False)
+    created_at = db.Column(db.DataTime, defaut=datetime.now())
+    updated_at = db.Column(db.DataTime, defaut=datetime.now())
