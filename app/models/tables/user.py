fd242f54d44b1c2f726b444e8ca144eae8412b94
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-01-17 00:19:25 -0300

    Fix: Adição do campo username na tabela e modificação do gitignore

--- app/models/tables/user.py
+++ app/models/tables/user.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=False, nullable=False)
+    username = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.Text, unique=False, nullable=False)
     created_at = db.Column(db.DataTime, defaut=datetime.now())
