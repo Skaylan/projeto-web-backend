c91152da2d9a0685072304a9561c5a409d423da5
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-01-23 18:17:53 -0300

    fix: Resolvendo erro critico com sqlaclhemy e o banco de dados

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,8 +1,7 @@
-import os
 from app import app
-from flask_cors import CORS
 from dotenv import load_dotenv
-
+import os
+from flask_cors import CORS
 
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
