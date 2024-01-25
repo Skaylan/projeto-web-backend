a4bb3d8bc35344adf357a8639d63d2688549d69c
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-01-25 00:23:53 -0300

    feat: Adicionando testes automatizados para as rotas create_user e delete_user

---
+++ tests/test_routes.py
@@ -0,0 +1,61 @@
+import requests
+from requests import Response
+
+ENDPOINT = 'http://localhost:5000/api/v1'
+
+
+def test_create_user():
+    payload = {
+        'name': 'test_name',
+        'username': 'test_username',
+        'email': 'test_email@email.com',
+        'password': 'testpassword',
+        're_password': 'testpassword',
+    }
+    
+    create_user_response = create_user(payload)
+    status = create_user_response.status_code
+    assert status == 201
+    
+    delete_user_payload = {
+        'username': payload.get('username'),
+        'password': payload.get('password')
+    }
+    
+    delete_user_response = delete_user(delete_user_payload)
+    assert delete_user_response.status_code == 200
+
+
+def test_delete_user():
+    payload = {
+        'name': 'test_name',
+        'username': 'test_username',
+        'email': 'test_email@email.com',
+        'password': 'testpassword',
+        're_password': 'testpassword',
+    }
+    
+    create_user_response = create_user(payload)
+    assert create_user_response.status_code == 201
+    
+    delete_payload = {
+        'username': payload.get('username'),
+        'password': payload.get('password')
+    }
+    
+    delete_user_response = delete_user(delete_payload)
+    assert delete_user_response.status_code == 200
+    
+    get_user_response = get_one_user(payload.get('username'))
+    assert get_user_response.status_code == 404
+    
+
+    
+def create_user(payload: dict) -> Response:
+    return requests.post(ENDPOINT + '/create_user', json=payload)
+
+def delete_user(payload: dict) -> Response:
+    return requests.delete(ENDPOINT + '/delete_user', json=payload)
+
+def get_one_user(username: str) -> Response:
+    return requests.get(ENDPOINT + '/get_one_user', params={'username': username})
