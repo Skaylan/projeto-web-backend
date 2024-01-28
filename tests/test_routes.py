077ebee5aa28a27900d586a86591c21472f2c46d
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2024-01-28 00:55:51 -0300

    Feat: Criação do teste da rota edit_category

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,7 +127,43 @@ def test_get_categories():
     delete_category_response = delete_category(delete)
     assert delete_category_response.status_code == 200
 
-    
+
+def test_edit_category():
+    payload = {
+        'name': 'testecategory'
+    }
+
+    create_category_response = create_category(payload)
+    assert create_category_response.status_code == 201
+
+    get_one_category_response = get_one_category(payload.get('name'))
+    assert get_one_category_response.status_code == 200
+
+    body = get_one_category_response.json()
+    assert body.get('category').get('name') == payload.get('name')
+
+    category_edit = {
+        'new_name': 'testecategory2',
+        'category_id': body.get('category').get('id')
+    }
+
+    edit_category_response = edit_category(category_edit)
+    assert edit_category_response.status_code == 200
+
+    get_one_category_response = get_one_category(category_edit.get('new_name'))
+    assert get_one_category_response.status_code == 200
+
+    body = get_one_category_response.json()
+    assert body.get('category').get('name') == category_edit.get('new_name')
+
+    delete = {
+        'id': body.get('category').get('id')
+    }
+
+    delete_category_response = delete_category(delete)
+    assert delete_category_response.status_code == 200
+
+
 def create_user(payload: dict) -> Response:
     return requests.post(ENDPOINT + '/create_user', json=payload)
 
@@ -151,3 +187,6 @@ def get_categories() -> Response:
 
 def get_one_category(name: str) -> Response:
     return requests.get(ENDPOINT + '/get_one_category', params={'name': name})
+
+def edit_category(payload: dict) -> Response:
+    return requests.put(ENDPOINT + '/edit_category', json=payload)
