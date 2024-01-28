046ddd784ad9f221eea0f48a814e17c706f303b6
leticosta4 <leticiacostaoa@gmail.com>
2024-01-27 21:16:25 -0300

    modelagem da tabela de filmes

---
+++ app/models/tables/movie.py
@@ -0,0 +1,40 @@
+from app.config.app_config import *
+from app.config.db_config import *
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, Integer, Double, ForeignKey
+from uuid import uuid4 
+
+class Movie(Base): 
+    id: Mapped[str] = mapped_column(String, primary_key=True, default=lambda: str(uuid4())) 
+    title: Mapped[str] = mapped_column(Text, unique=False, nullable=False)
+    original_title: Mapped[str] = mapped_column(Text, unique=False, nullable=False)
+    romanised_original_title: Mapped[str] = mapped_column(Text, unique=False, nullable=False)
+    description: Mapped[str] = mapped_column(Text, unique=True, nullable=False) 
+    studio: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    director: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    rating: Mapped[Double] = mapped_column(Double, unique=True, nullable=False) 
+    banner_img_id: Mapped[str] = mapped_column(Text, unique=True, nullable=False) 
+    launch_date: Mapped[str] = mapped_column(String(45), unique=True, nullable=False)
+    cover_img_id: Mapped[str] = mapped_column(Text, unique=True, nullable=False) 
+    producer: Mapped[str] = mapped_column(String(100), unique=True, nullable=False) 
+    running_time: Mapped[int] = mapped_column(Integer, unique=True, nullable=False) 
+
+    category_id: Mapped[str] = mapped_column(String, ForeignKey("category.id"), unique=True, nullable=False) 
+
+    def __init__(self, title: str, original_title: str, romanised_original_title: str, description: str, studio: str, director: str,
+                 rating: Double, banner_img_id: str, launch_date: str, cover_img_id: str, producer: str, running_time: int,
+                 category_id: str
+                 ):
+        self.title = title
+        self.original_title = original_title
+        self.romanised_original_title = romanised_original_title
+        self.description = description
+        self.studio = studio
+        self.director = director
+        self.rating = rating
+        self.banner_img_id = banner_img_id
+        self.launch_date = launch_date
+        self.cover_img_id = cover_img_id
+        self.producer = producer
+        self.running_time = running_time
+        self.category_id = category_id
