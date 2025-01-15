3b510ac7a9566d1fd904313e004f2c7312517c7c
Carlos Erick <carloserico71@gmail.com>
2025-01-15 11:58:09 -0300

    feat: tabelas de serialização de session

--- app/models/schemas/session_schema.py
+++ app/models/schemas/session_schema.py
@@ -1,8 +1,11 @@
 from app.extensions import ma
 from app.models.tables.session import Session
+from app.models.tables.user import User
 
 
 class SessionSchema(ma.SQLAlchemyAutoSchema):
+
+    users = ma.Nestad(User)
     class Meta:
         model = Session
         load_instance = True
--- app/models/schemas/user_schema.py
+++ app/models/schemas/user_schema.py
@@ -1,10 +1,8 @@
 from app.extensions import ma
 from app.models.tables.user import User
-from app.models.schemas.session_schema import Session
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
-    session = ma.Nested(Session)
     class Meta:
         model = User
         load_instance = True
