1211ee28c3c2ad942c3cb83be0d22defa3ec0344
carlos <carloserico71@gmail.com>
2025-01-24 00:19:41 -0300

    fix: corrigindo a serialização dos dados dos filmes

--- app/models/schemas/category_movie_schema.py
+++ app/models/schemas/category_movie_schema.py
@@ -1,12 +1,12 @@
 from app.extensions import ma
 from app.models.tables.movie_category import MovieCategory
-from app.models.tables.category import Category
 from app.models.schemas.category_schema import CategorySchema
+from app.models.schemas.movie_schema import MovieSchema
+
 
 class MovieCategorySchema(ma.SQLAlchemyAutoSchema):
-    
-    Category = ma.Nested(CategorySchema)
-
+    movie = ma.Nested(MovieSchema)
+    category = ma.Nested(CategorySchema)
     class Meta:
         model = MovieCategory
         load_instance = True
